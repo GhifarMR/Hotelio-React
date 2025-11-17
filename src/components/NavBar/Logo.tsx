ec7542849e6078fb246efcb647097ef05e8f787e
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-11-17 19:34:40 +0700

    repair the logo

--- src/components/NavBar/Logo.tsx
+++ src/components/NavBar/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo: React.FC = () => (
-  <div className="font-bold cursor-pointer text-2xl ml-6 hover:bg-purple-950 hover:text-white pt-1 pb-1 pl-1 pr-1">
+  <div className="font-bold cursor-pointer text-2xl ml-6 md:hover:bg-purple-950 md:hover:text-white active:bg-purple-950 active:text-white pt-1 pb-1 pl-1 pr-1">
     <a href="/">HOTELIO</a>
   </div>
 );
